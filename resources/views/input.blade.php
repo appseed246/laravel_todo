3499bc361a9208f2baf75a1a1909179fcde7beb0
appseed246 <guitar.telecaster2017@gmail.com>
2018-06-01 02:52:28 +0900

    bootstrap導入

--- resources/views/input.blade.php
+++ resources/views/input.blade.php
@@ -1,67 +1,66 @@
 @extends('master') 
 
-@section('title')
-Todoリスト
-@stop
+@section('title', 'Todoリスト')
 
 @section('body')
-<h1>Todo List</h1>
-<div>
+<div class="container">
+    <h1 class="text-center">Todo List</h1>
     @if (session('status'))
-        <div>
+        <div class="alert alert-info">
             {{session('status')}}
         </div>
-        <br>
     @endif
-</div>
-<div>
-    <form action="{{action('IndexController@confirm')}}" method="post">
-        <div>
-            名前: <input type="input" name="name" value="{{old('name')}}">
-            <div>{{$errors->first('name')}}</div>
-        </div>
-        <br>
-        <div>
-            タスク: <input type="input" name="content" value="{{old('content')}}">
-            <div>{{$errors->first('content')}}</div>
-        </div>
-        <br>
-        <div>
-        期限: <input type="text" name="limit" value="{{old('limit')}}">
-        <div>{{$errors->first('limit')}}</div>
-        {{-- <input type="hidden" name="_token" value="{{csrf_token()}}"> --}}
+    <form class="form-horizontal" action="{{action('IndexController@confirm')}}" method="post">
         {{ csrf_field() }}
-        <input type="submit" value="内容確認">
+        <div class="form-group">
+            <label for="name" class="control-label">名前</label>
+            <div>
+                <input type="input" class="form-control" name="name" value="{{old('name')}}">
+            </div>
+            <div class="text-danger">{{$errors->first('name')}}</div>
+        </div>
+        <div class="form-group">
+            <label for="content" class="control-label">タスク</label>
+            <div>
+                <input type="input" class="form-control" name="content" value="{{old('content')}}">
+            </div>
+            <div class="text-danger">{{$errors->first('content')}}</div>
+        </div>
+        <div class="form-group">
+            <label for="limit" class="control-label">期限</label>
+            <div>
+                <input type="text" class="form-control" name="limit" value="{{old('limit')}}">
+            </div>
+            <div class="text-danger">{{$errors->first('limit')}}</div>
+        </div>
+        <button type="submit" class="btn btn-default text-left">内容確認</button>
     </form>
-</div>
-{{-- @php
-var_dump($tasks);
-@endphp --}}
-<br>
-<div>
-    <table>
+    <br>
+    <table class="table table-striped">
         <thead>
-            <th>登録者</th>
-            <th>タスク</th>
-            <th>期限</th>
+            <th scope="col">No.</th>
+            <th scope="col">登録者</th>
+            <th scope="col">タスク</th>
+            <th scope="col">期限</th>
         </thead>
         <tbody>
             @foreach ($tasks as $task)
             <tr>
-                <td>
-                    <div>{{ $task->name }}</div>
+                <th scope="row">
+                    {{ $task->id }}
                 </td>
                 <td>
-                    <div>{{ $task->content }}</div>
+                    {{ $task->name }}
                 </td>
                 <td>
-                    <div>
-                        @if(is_null($task->limit))
-                            なし
-                        @else
-                            {{ $task->limit }}
-                        @endif
-                    </div>
+                    {{ $task->content }}
+                </td>
+                <td>
+                    @if(is_null($task->limit))
+                        なし
+                    @else
+                        {{ $task->limit }}
+                    @endif
                 </td>
             </tr>
             @endforeach
